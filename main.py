eb04bd495ec29a244de161f5b4184359ae8cf2e2
Tom Zhang <tianze.zhang@gmail.com>
2022-03-27 20:03:16 +0100

    removed uneccessary parts T.T

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
 import os
 import uvicorn
-import csv 
-import json
-from pydantic import BaseModel
-from fastapi.responses import FileResponse
 from pickle import load
 import pandas as pd
 import math
@@ -23,42 +19,6 @@ def predict(bathroomcnt : int, bedroomcnt: int, finishedsquarefeet12:float,taxam
     Zestimate=math.exp(math.log(house_price)+logerror)
     return {"log error":logerror,"Pridicted house price":Zestimate}
 
-@app.get('/sample-inputs')
-def get_sample_inputs_csv():
-    csv_file_path = os.path.join(path,'finalDataset.csv')
-    if os.path.exists(csv_file_path):
-        return FileResponse('finalDataset.csv')
-    return {"error" : "File not found!"}
-
-@app.get('/sample-inputs-JSON-array')
-def sample_inputs_JSON_arr():
-    jsonArray = []
-    csv_file_path = os.path.join(path,'finalDataset.csv')
-
-    if os.path.exists(csv_file_path):
-        with open(csv_file_path, encoding='utf-8') as csvf:
-            csvReader = csv.DictReader(csvf)
-            for row,element in enumerate(csvReader):
-                if row < 100:
-                  jsonArray.append(element)
-
-        return jsonArray
-
-@app.get('/sample-inputs-JSON')
-def sample_inputs_JSON():
-    jsonArray = []
-    csv_file_path = os.path.join(path,'finalDataset.csv')
-
-    if os.path.exists(csv_file_path):
-        with open(csv_file_path, encoding='utf-8') as csvf:
-            csvReader = csv.DictReader(csvf)
-            for row in csvReader:
-                  jsonArray.append(row)
-        with open(r'data.json', 'w', encoding='utf-8') as jsonf: 
-            jsonString = json.dumps(jsonArray, indent=4)
-            jsonf.write(jsonString)
-        return FileResponse('data.json')
-    return {"error" : "File not found!"}
 
 
 
