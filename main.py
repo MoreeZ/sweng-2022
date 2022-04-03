78b58d2dfd3123191e0b7118c4568a94bfe73b29
Tom Zhang <tianze.zhang@gmail.com>
2022-04-03 15:01:12 +0100

    Allowing all origins

--- main.py
+++ main.py
@@ -9,14 +9,10 @@ import math
 from fastapi.middleware.cors import CORSMiddleware
 # Creating FastAPI instance
 app = FastAPI()
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "https://predict-houseprice.web.app",
-]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
